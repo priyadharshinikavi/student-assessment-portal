331941d10530a301412f2e019a165a0d49d5bc61
Priyadharshini Kavi <kavi@example.com>
2026-09-24 17:59:35 +0530

    Update Firebase Config with production keys

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,14 +1,12 @@
-// js/firebase-config.js
-
-// TODO: Replace this with your actual Firebase config object
-// You can get this by creating a project at https://console.firebase.google.com
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCdN-vcFza46CdSWTizokpV-4c2mPdaEyc",
+  authDomain: "priya-developer.firebaseapp.com",
+  projectId: "priya-developer",
+  storageBucket: "priya-developer.firebasestorage.app",
+  messagingSenderId: "283917047303",
+  appId: "1:283917047303:web:20d3f6ac0a5780fdb0fca2",
+  measurementId: "G-CV4B8W93BC"
 };
 
 // Initialize Firebase
